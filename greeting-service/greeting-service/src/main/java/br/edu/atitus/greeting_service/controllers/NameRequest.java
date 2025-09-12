f50b51d6960c3efab3442410a2eb177c29ecb8f1
milena-aalves <milenadeabreualves@gmail.com>
2025-09-12 11:17:30 -0300

    Atividade Ajustes no Greeting Service

---
+++ greeting-service/greeting-service/src/main/java/br/edu/atitus/greeting_service/controllers/NameRequest.java
@@ -0,0 +1,13 @@
+package br.edu.atitus.greeting_service.controllers;
+
+public class NameRequest {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
